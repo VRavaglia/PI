14e5e65c3755f1735781686b40af406825be2ece
Mateus da Silva Gilbert <gilbertmateus@poli.ufrj.br>
2021-04-15 16:12:17 -0300

    extração de parâmetros gpu

--- BD/classesProdutos.py
+++ BD/classesProdutos.py
@@ -1,85 +1,104 @@
 class Produto:
-	def __init__(self, nome, preco, preco_desconto, link, modelo, marca):
+	def __init__(self, site, nome, preco, preco_desconto, link, modelo, marca, info_adicionais):
+		self.site = site
 		self.nome = nome
 		self.preco = preco
 		self.preco_desconto = preco_desconto
 		self.link = link
 		self.modelo = modelo
 		self.marca = marca
+		self.info_ad = info_adicionais
 
 	def parametros(self):
+		print('Site: {}'.format(self.site))
 		print('Nome: {}'.format(self.nome))
 		print('Preco: R${}'.format(self.preco))
 		print('Desconto: R${}'.format(self.preco_desconto))
-		print('Link: {}'.format(self.link))
+		print('Link: {}'.format(self.site + self.link))
 		print('Modelo: {}'.format(self.modelo))
 		print('Marca: {}'.format(self.marca))
 
+	def info_adicionais(self):
+		print('Info Adicionais: {}'.format(self.info_ad))
+
 class Mobo(Produto):
-    def __init__(self, site, nome, preco, preco_desconto, link, modelo, marca, chipset, socket, tamanho, ddr):
-        super().__init__(site, nome, preco, preco_desconto, link, modelo, marca)
-        self.chipset = chipset
-        self.socket = socket
-        self.tamanho = tamanho
-        self.ddr = ddr
-        self.tipo = 'Mobo'
+	def __init__(self, site, nome, preco, preco_desconto, link, modelo, marca, chipset, socket, tamanho, ddr):
+		super().__init__(site, nome, preco, preco_desconto, link, modelo, marca)
+		self.chipset = chipset
+		self.socket = socket
+		self.tamanho = tamanho
+		self.ddr = ddr
+		self.tipo = 'Mobo'
 
 class Psu(Produto):
-    def __init__(self, site, nome, preco, preco_desconto, link, modelo, marca, selo, potencia, modularidade):
-        super().__init__(site, nome, preco, preco_desconto, link, modelo, marca)
-        self.selo = selo
-        self.potencia = potencia
-        self.modulariade = modulariade
-        self.tipo = 'Psu'
+	def __init__(self, site, nome, preco, preco_desconto, link, modelo, marca, selo, potencia, modularidade):
+		super().__init__(site, nome, preco, preco_desconto, link, modelo, marca)
+		self.selo = selo
+		self.potencia = potencia
+		self.modulariade = modulariade
+		self.tipo = 'Psu'
 
 class Cpu(Produto):
-    def __init__(self, site, nome, preco, preco_desconto, link, modelo, marca, socket, frequencia, integrada):
-        super().__init__(site, nome, preco, preco_desconto, link, modelo, marca)
-        self.socket = socket
-        self.frequencia = frequencia
-        self.integrada = integrada
-        self.tipo = 'Cpu'
-
+	def __init__(self, site, nome, preco, preco_desconto, link, modelo, marca, socket, frequencia, integrada):
+		super().__init__(site, nome, preco, preco_desconto, link, modelo, marca)
+		self.socket = socket
+		self.frequencia = frequencia
+		self.integrada = integrada
+		self.tipo = 'Cpu'
 
 class Ram(Produto):
-    def __init__(self, site, nome, preco, preco_desconto, link, modelo, marca, capacidade, frequencia, ddr, latencia, quantidade):
-        super().__init__(site, nome, preco, preco_desconto, link, modelo, marca)
-        self.capacidade = capacidade
-        self.frequencia = frequencia
-        self.ddr = ddr
-        self.latencia = latencia
-        self.quantidade = quantidade
-        self.tipo = 'Ram'
+	def __init__(self, site, nome, preco, preco_desconto, link, modelo, marca, capacidade, frequencia, ddr, latencia, quantidade,info_adicionais):
+		super().__init__(site, nome, preco, preco_desconto, link, modelo, marca,info_adicionais)
+		self.capacidade = capacidade
+		self.frequencia = frequencia
+		self.ddr = ddr
+		self.latencia = latencia
+		self.quantidade = quantidade
+		self.tipo = 'Ram'
 
 	def parametros(self):
 		super().parametros()
 		print('Capacidade: {}'.format(self.capacidade))
 		print('Frequência: {}'.format(self.frequencia))
-		print('Tamanho: {}'.format(self.tamanho))
 		print('DDR: {}'.format(self.ddr))
 		print('Latencia: {}'.format(self.latencia))
 		print('Quantidade: {}'.format(self.quantidade))
+		if self.info_ad:
+			self.info_adicionais()
+
+	def info_adicionais(self):
+		super().info_adicionais()
 
 class Hd(Produto):
-    def __init__(self, site, nome, preco, preco_desconto, link, modelo, marca, dimensoes, ssd, nvme, sata):
-        super().__init__(site, nome, preco, preco_desconto, link, modelo, marca)
-        self.dimensoes = dimensoes
-        self.ssd = ssd
-        self.nvme = nvme
-        self.sata = sata
-        self.tipo = 'Hd'
+	def __init__(self, site, nome, preco, preco_desconto, link, modelo, marca, dimensoes, ssd, nvme, sata):
+		super().__init__(site, nome, preco, preco_desconto, link, modelo, marca)
+		self.dimensoes = dimensoes
+		self.ssd = ssd
+		self.nvme = nvme
+		self.sata = sata
+		self.tipo = 'Hd'
 
 
 class Gpu(Produto):
-    def __init__(self, site, nome, preco, preco_desconto, link, modelo, marca, fabricante, vram):
-        super().__init__(site, nome, preco, preco_desconto, link, modelo, marca)
-        self.fabricante = fabricante
-        self.vram = vram
-        self.tipo = 'Gpu'
+	def __init__(self, site, nome, preco, preco_desconto, link, modelo, marca, fabricante, vram, info_adicionais):
+		super().__init__(site, nome, preco, preco_desconto, link, modelo, marca,info_adicionais)
+		self.fabricante = fabricante
+		self.vram = vram
+		self.tipo = 'Gpu'
+
+	def parametros(self):
+		super().parametros()
+		print('Fabricante: {}'.format(self.fabricante))
+		print('VRam: {}'.format(self.vram))
+		if self.info_ad:
+			self.info_adicionais()
+
+	def info_adicionais(self):
+		super().info_adicionais()
 
 
 class Case(Produto):
-    def __init__(self, site, nome, preco, preco_desconto, link, modelo, marca, fabricante, tamanho):
-        super().__init__(site, nome, preco, preco_desconto, link, modelo, marca)
-        self.tamanho = fabricant
-        self.tipo = 'Case'
+	def __init__(self, site, nome, preco, preco_desconto, link, modelo, marca, fabricante, tamanho):
+		super().__init__(site, nome, preco, preco_desconto, link, modelo, marca)
+		self.tamanho = fabricante
+		self.tipo = 'Case'
--- InfoExt/manager.py
+++ InfoExt/manager.py
@@ -11,7 +11,8 @@ brands = pBrands + ['cooler_master', 'evga', 'corsair', 'thermaltake', 'pcyes', 
 			'aerocool', 'redragon', 'patriot', 'hyperx', 'crucial', 'msi', 'galax', 'pny',
 			'deepcool', 'afox','colorful', 'biostar', 'seagate', 'sandisk', 'kingston',
 			'western_digital', 'toshiba', 'hikvision', 'nzxt', 'lian_li', 'xpg', 'husky',
-			'hp', 'lexar', 'ocpc', 'adata', 'g_skill']#, 'Unknown']
+			'hp', 'lexar', 'ocpc', 'adata', 'g_skill', 'asus', 'gainward', 'zotac',
+			'sapphire', 'asrock', 'xfx', 'powercolor']#, 'Unknown']
 
 def initCat(brands):
 	catalog = dict()
@@ -28,13 +29,12 @@ def manageProd(products,plist):
 		link = product['link']
 		marca = exMarca(re.sub(r',',r' ',nome),brands)
 		if p_type == 'ram':
-			capacidade = None
+			capacidade = re.search(r'[1-9]([0-9]+)?(GB|TB)', nome)
+			if capacidade:
+				capacidade = capacidade.group()
 			frequencia = re.search(r'[1-9]([0-9]+)?(\s)?M(Hz|hz)', nome)
 			if frequencia:
 				frequencia = frequencia.group()
-			tamanho = re.search(r'[1-9]([0-9]+)?(GB|TB)', nome)
-			if tamanho:
-				tamanho = tamanho.group()
 			ddr = re.search(r'DDR(-)?[1-9]([0-9]+)?(-[0-9]+)?', nome)
 			if ddr:
 				ddr = ddr.group()
@@ -42,18 +42,20 @@ def manageProd(products,plist):
 			if latencia:
 				latencia = latencia.group()
 			quantidade = None
-			modelo = nome.split(',')[0]
-			aux = normString(modelo,marca)
+			modelo = nome.split(' - ')[-1]
+			info_adicionais = nome.split(',')[0]
+			aux = normString(info_adicionais,marca)
 			if aux:
-				modelo = aux
+				info_adicionais = aux
 			if frequencia:
-				modelo = re.sub(frequencia, '', modelo)
-			modelo = modelo.split(' ')
-			modelo = [i for i in modelo if i.lower() != marca]
-			removables = [ddr, latencia, tamanho]			#mais comportados
-			modelo = ' '.join([i for i in modelo if i not in removables])
+				info_adicionais = re.sub(frequencia, '', info_adicionais)
+			info_adicionais = info_adicionais.split(' ')
+			info_adicionais = [i for i in info_adicionais if i.lower() != marca]
+			removables = [ddr, latencia, capacidade, modelo]			#mais comportados
+			info_adicionais = ' '.join([i for i in info_adicionais if i not in removables])
 
-			p_list.append(Ram(nome,
+			p_list.append(Ram('kabum.com.br',#site,
+									nome,
 									preco,
 									desconto,
 									link,
@@ -61,19 +63,59 @@ def manageProd(products,plist):
 									marca,
 									capacidade,
 									frequencia,
-									tamanho,
 									ddr,
 									latencia,
-									quantidade))
+									quantidade,
+									info_adicionais))
+
+		elif p_type == 'gpu':
+			fabricante = exMarca(re.sub(r',',r' ',nome), pBrands)
+			if fabricante == 'Unknown':
+				if re.search(r'(^|\s)(radeon|r(x|9)(\s)?(39|55|68)00?)', nome, flags=re.IGNORECASE):#talvez refinar
+					fabricante = 'amd'
+				elif re.search(r'(^|\s)(geforce|(gtx?)(\s)?7[0-9]0)', nome, flags=re.IGNORECASE):#talvez refinar
+					fabricante = 'nvidia'
+			marca = exMarca(re.sub(r',',r' ',nome),[i for i in brands if i not in pBrands])
+			vram = re.search(r'[1-9]([0-9]+)?GB', nome)
+			if not vram:
+				vram = re.search(r'[1-9]([0-9]+)?G', nome)
+			if vram:
+				vram = vram.group()
+
+			modelo = nome.split(' - ')[-1]
+			info_adicionais = nome.split(',')[0]
+			aux = normString(info_adicionais,fabricante)
+			if aux:
+				info_adicionais = aux
+			info_adicionais = info_adicionais.split(' ')
+			info_adicionais = [i for i in info_adicionais if (i.lower() != marca or i.lower != fabricante)]
+			removables = [vram]			#mais comportados
+			info_adicionais = ' '.join([i for i in info_adicionais if i not in removables])
+
+			p_list.append(Gpu('kabum.com.br',#site,
+									nome,
+									preco,
+									desconto,
+									link,
+									modelo,
+									marca,
+									fabricante,
+									vram,
+									info_adicionais))
 
 if __name__ == '__main__':
+	from random import randint
+
 	#catalogo = initCat(brands)
-	print(brands)
-	p_type = 'ram'
+	#print(brands)
+	p_type = 'gpu'#'ram'
 	pecas = main_bypasser(p_type)
-	ram_list = (p_type, list())
-	manageProd(pecas,ram_list)
-	for prod in ram_list[1][:10]:
+	pprint(pecas[:10])
+	prod_list = (p_type, list())
+	manageProd(pecas,prod_list)
+	m_val = int(len(prod_list[1])/10)
+	a = randint(1,m_val-1)
+	for prod in prod_list[1][(a-1)*10:a*10]:
 		#if prod.marca == 'Unknown':
 		prod.parametros()
 		#pprint(vars(prod))
